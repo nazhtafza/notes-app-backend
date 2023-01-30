76d4c96f0c573b3aecd67059216d6062d83385f9
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-01-30 21:05:11 +0700

    update

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,7 @@
 const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
+// menambahkan note
 const addNoteHandler = (request, h) => {
     const { title, tags, body } = request.payload;
 
@@ -42,6 +43,7 @@ const addNoteHandler = (request, h) => {
 
 };
 
+// membaca note
 const getAllNotesHandler = () => ({
     status: 'success',
     data: {
@@ -69,4 +71,36 @@ const getNoteByIdHandler = (request, h) => {
     response.code(404);
     return response;
 };
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
+// mengupdate note
+const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const { title, tags, body } = request.payload;
+    const updatedAt = new Date().toISOString();
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updatedAt,
+        };
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil diperbarui',
+        });
+        response.code(200);
+        return response;
+    }
+    const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler };
